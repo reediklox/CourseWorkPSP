1200026652a87f951a09d5e4d8a451f6ed97e4ca
reediklox <deerikcosmos@mail.ru>
2023-12-12 20:04:33 +0300

    update

---
+++ src/main/java/Client/Controllers/User/ComparisonWin.java
@@ -0,0 +1,8 @@
+package Client.Controllers.User;
+
+import javafx.scene.control.Button;
+
+public class ComparisonWin {
+    public Button Calcs;
+    public Button DataLists;
+}
--- src/main/java/Client/Controllers/User/UserWindow.java
+++ src/main/java/Client/Controllers/User/UserWindow.java
@@ -1,15 +1,29 @@
 package Client.Controllers.User;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 public class UserWindow {
+    @FXML
     public Button Calcs;
+
+    @FXML
     public Button DataLists;
+
+    @FXML
     public Button Analitics;
+
+    @FXML
     public Button Forecasting;
+
+    @FXML
     public Button Comparison;
+
+    @FXML
     public Text UserLogin;
+
+    @FXML
     public ImageView CloseButton;
 }
